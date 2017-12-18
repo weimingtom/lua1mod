9b9eb383e5f848eafcd1f9a8d1d4dfc6a9339f2d
weimingtom <weimingtom@qq.com>
2017-12-18 22:01:09 +0800

    mod

--- csharp/y_tab.c.cs
+++ csharp/y_tab.c.cs
@@ -1,7 +1,6 @@
 ﻿//#define YYDEBUG
 
 using System;
-using System.Runtime.InteropServices;
 
 //FIXME:TODO:ptr class var value assign
 namespace KopiLua
@@ -645,7 +644,13 @@ namespace KopiLua
 			15, 24, 0, 82, 0, 0, 17, 13, 32, 84, 
 			90, 31, 26, 32, 23, 25
 		};
-		public class yytoktype {public CharPtr t_name; public int t_val; }	
+		public class yytoktype {public CharPtr t_name; public int t_val; 
+			public yytoktype(CharPtr t_name, int t_val) 
+			{ 
+				this.t_name = t_name; 
+				this.t_val = t_val;
+			} 
+		}
 //		#ifndef YYDEBUG
 //		#	define YYDEBUG	0	/* don't allow debugging */
 //		#endif
@@ -809,7 +814,7 @@ namespace KopiLua
 		*/
 //		#define YYERROR		goto yyerrlab
 		public static int YYACCEPT()	{ free(yys); free(yyv); return(0); }
-//		#define YYABORT		{ free(yys); free(yyv); return(1); }
+		public static int YYABORT()		{ free(yys); free(yyv); return(1); }
 //		#define YYBACKUP( newtoken, newvalue )\
 //		{\
 //			if ( yychar >= 0 || ( yyr2[ yytmp ] >> 1 ) != 1 )\
@@ -1029,14 +1034,14 @@ yydefault:
 			if ( ( yy_n = yydef[ yy_state ] ) == -2 )
 			{
 #if YYDEBUG
-				yytmp = yychar < 0;
+				yytmp = (yychar < 0 ? 1 : 0);
 #endif
 				if ( ( yychar < 0 ) && ( ( yychar = yylex() ) < 0 ) )
 					yychar = 0;		/* reached EOF */
 #if YYDEBUG
-				if ( yydebug && yytmp )
+				if ( yydebug != 0 && yytmp != 0 )
 				{
-					register int yy_i;
+					int yy_i;
 	
 					printf( "Received token " );
 					if ( yychar == 0 )
@@ -1080,173 +1085,172 @@ yydefault:
 
 
 				
-//					/*
-//					** check for syntax error
-//					*/
-//					if (yy_n == 0) // have an error
-//					{
-//						/* no worry about speed here! */
-//						switch (yyerrflag)
-//						{
-//						case 0: // new error
-//							yyerror("syntax error");
-//							goto skip_init;
-//						yyerrlab:
-//							/*
-//							** get globals into registers.
-//							** we have a user generated syntax type error
-//							*/
-//							yy_pv = yypv;
-//							yy_ps = yyps;
-//							yy_state = yystate;
-//							yynerrs++;
-//						skip_init:
-//		//C++ TO C# CONVERTER TODO TASK: C# does not allow fall-through from a non-empty 'case':
-//						case 1:
-//						case 2: // incompletely recovered error
-//								/* try again... */
-//							yyerrflag = 3;
-//							/*
-//							** find state where "error" is a legal
-//							** shift action
-//							*/
-//							while (yy_ps >= yys)
-//							{
-//								yy_n = yypact[yy_ps] + YYERRCODE;
-//								if (yy_n >= 0 && yy_n < YYLAST && yychk[yyact[yy_n]] == YYERRCODE)
-//								{
-//									/*
-//									** simulate shift of "error"
-//									*/
-//									yy_state = yyact[yy_n];
-//									goto yy_stack;
-//								}
-//								/*
-//								** current state has no shift on
-//								** "error", pop stack
-//								*/
-//			#if YYDEBUG
-//			# define _POP_ "Error recovery pops state %d, uncovers state %d\n"
-//								if (yydebug)
-//								{
-//									printf(_POP_, yy_ps, yy_ps[-1]);
-//								}
-//			# undef _POP_
-//			#endif
-//								yy_ps--;
-//								yy_pv--;
-//							}
-//							/*
-//							** there is no state on stack with "error" as
-//							** a valid shift.  give up.
-//							*/
-//							YYABORT;
-//		//C++ TO C# CONVERTER TODO TASK: C# does not allow fall-through from a non-empty 'case':
-//						case 3: // no shift yet; eat a token
-//			#if YYDEBUG
-//							/*
-//							** if debugging, look up token in list of
-//							** pairs.  0 and negative shouldn't occur,
-//							** but since timing doesn't matter when
-//							** debugging, it doesn't hurt to leave the
-//							** tests here.
-//							*/
-//							if (yydebug)
-//							{
-//		//C++ TO C# CONVERTER NOTE: 'register' variable declarations are not supported in C#:
-//		//ORIGINAL LINE: register int yy_i;
-//								int yy_i;
-//		
-//								Console.Write("Error recovery discards ");
-//								if (yychar == 0)
-//								{
-//									Console.Write("token end-of-file\n");
-//								}
-//								else if (yychar < 0)
-//								{
-//									Console.Write("token -none-\n");
-//								}
-//								else
-//								{
-//									for (yy_i = 0; yytoks[yy_i].t_val >= 0; yy_i++)
-//									{
-//										if (yytoks[yy_i].t_val == yychar)
-//										{
-//											break;
-//										}
-//									}
-//									Console.Write("token {0}\n", yytoks[yy_i].t_name);
-//								}
-//							}
-//			#endif
-//							if (yychar == 0) // reached EOF. quit
-//							{
-//								YYABORT;
-//							}
-//							yychar = -1;
-//							goto yy_newstate;
-//						}
-//					} // end if ( yy_n == 0 )
-//					/*
-//					** reduction by production yy_n
-//					** put stack tops, etc. so things right after switch
-//					*/
-//			#if YYDEBUG
-//					/*
-//					** if debugging, print the string that is the user's
-//					** specification of the reduction which is just about
-//					** to be done.
-//					*/
-//					if (yydebug)
-//					{
-//						Console.Write("Reduce by ({0:D}) \"{1}\"\n", yy_n, yyreds[yy_n]);
-//					}
-//			#endif
-//					yytmp = yy_n; // value to switch over
-//		//C++ TO C# CONVERTER TODO TASK: C# does not have an equivalent to pointers to variables (in C#, the variable no longer points to the original when the original variable is re-assigned):
-//		//ORIGINAL LINE: yypvt = yy_pv;
-//					yypvt = yy_pv; // $vars top of value stack
-//					/*
-//					** Look in goto table for next state
-//					** Sorry about using yy_state here as temporary
-//					** register variable, but why not, if it works...
-//					** If yyr2[ yy_n ] doesn't have the low order bit
-//					** set, then there is no action to be done for
-//					** this reduction.  So, no saving & unsaving of
-//					** registers done.  The only difference between the
-//					** code just after the if and the body of the if is
-//					** the goto yy_stack in the body.  This way the test
-//					** can be made before the choice of what to do is needed.
-//					*/
-//					{
-//						/* length of production doubled with extra bit */
-//		//C++ TO C# CONVERTER NOTE: 'register' variable declarations are not supported in C#:
-//		//ORIGINAL LINE: register int yy_len = yyr2[yy_n];
-//						int yy_len = yyr2[yy_n];
-//		
-//						if (((yy_len & 0x1) == 0)
-//						{
-//							yy_len >>= 1;
-//							yyval = (yy_pv -= yy_len)[1]; // $$ = $1
-//							yy_state = yypgo[yy_n = yyr1[yy_n]] + *(yy_ps -= yy_len) + 1;
-//							if (yy_state >= YYLAST || yychk[yy_state = yyact[yy_state]] != -yy_n)
-//							{
-//								yy_state = yyact[yypgo[yy_n]];
-//							}
-//							goto yy_stack;
-//						}
-//						yy_len >>= 1;
-//						yyval = (yy_pv -= yy_len)[1]; // $$ = $1
-//						yy_state = yypgo[yy_n = yyr1[yy_n]] + *(yy_ps -= yy_len) + 1;
-//						if (yy_state >= YYLAST || yychk[yy_state = yyact[yy_state]] != -yy_n)
-//						{
-//							yy_state = yyact[yypgo[yy_n]];
-//						}
-//					}
-//								/* save until reenter driver code */
-//					yystate = yy_state;
-//					yyps = yy_ps;
-//					yypv = yy_pv;
+			/*
+			** check for syntax error
+			*/
+			if (yy_n == 0) // have an error
+			{
+				/* no worry about speed here! */
+				switch (yyerrflag)
+				{
+				case 0: // new error
+					yyerror("syntax error");
+					goto case 1; //goto skip_init;
+//yyerrlab:
+					/*
+					** get globals into registers.
+					** we have a user generated syntax type error
+					*/
+					yy_pv = yypv;
+					yy_ps = yyps;
+					yy_state = yystate;
+					yynerrs++;
+					goto case 1;
+//skip_init:
+				case 1:
+				case 2: 	/* incompletely recovered error */
+						/* try again... */
+					yyerrflag = 3;
+					/*
+					** find state where "error" is a legal
+					** shift action
+					*/
+					while (yy_ps >= yys)
+					{
+						yy_n = yypact[yy_ps[0]] + YYERRCODE;
+						if (yy_n >= 0 && yy_n < YYLAST && yychk[yyact[yy_n]] == YYERRCODE)
+						{
+							/*
+							** simulate shift of "error"
+							*/
+							yy_state = yyact[yy_n];
+							goto yy_stack;
+						}
+						/*
+						** current state has no shift on
+						** "error", pop stack
+						*/
+#if YYDEBUG
+						string _POP_ = "Error recovery pops state %d, uncovers state %d\n";
+						if (yydebug != 0)
+						{
+							printf(_POP_, yy_ps, yy_ps[-1]);
+						}
+						//# undef _POP_
+#endif
+						yy_ps.dec();
+						yy_pv.dec();
+					}
+					/*
+					** there is no state on stack with "error" as
+					** a valid shift.  give up.
+					*/
+					return YYABORT();
+					
+				case 3: // no shift yet; eat a token
+#if YYDEBUG
+					/*
+					** if debugging, look up token in list of
+					** pairs.  0 and negative shouldn't occur,
+					** but since timing doesn't matter when
+					** debugging, it doesn't hurt to leave the
+					** tests here.
+					*/
+					if ( yydebug != 0 )
+					{
+						int yy_i;
+	
+						printf( "Error recovery discards " );
+						if ( yychar == 0 )
+							printf( "token end-of-file\n" );
+						else if ( yychar < 0 )
+							printf( "token -none-\n" );
+						else
+						{
+							for ( yy_i = 0;
+								yytoks[yy_i].t_val >= 0;
+								yy_i++ )
+							{
+								if ( yytoks[yy_i].t_val
+									== yychar )
+								{
+									break;
+								}
+							}
+							printf( "token %s\n",
+								yytoks[yy_i].t_name );
+						}
+					}
+#endif
+					if (yychar == 0) // reached EOF. quit
+					{
+						return YYABORT();
+					}
+					yychar = -1;
+					goto yy_newstate;
+				}
+			} // end if ( yy_n == 0 )
+			
+			/*
+			** reduction by production yy_n
+			** put stack tops, etc. so things right after switch
+			*/
+#if YYDEBUG
+			/*
+			** if debugging, print the string that is the user's
+			** specification of the reduction which is just about
+			** to be done.
+			*/
+			if ( yydebug != 0 )
+				printf( "Reduce by (%d) \"%s\"\n",
+					yy_n, yyreds[ yy_n ] );
+#endif
+			yytmp = yy_n;			/* value to switch over */
+			yypvt = yy_pv;			/* $vars top of value stack */
+			/*
+			** Look in goto table for next state
+			** Sorry about using yy_state here as temporary
+			** register variable, but why not, if it works...
+			** If yyr2[ yy_n ] doesn't have the low order bit
+			** set, then there is no action to be done for
+			** this reduction.  So, no saving & unsaving of
+			** registers done.  The only difference between the
+			** code just after the if and the body of the if is
+			** the goto yy_stack in the body.  This way the test
+			** can be made before the choice of what to do is needed.
+			*/
+			{
+				/* length of production doubled with extra bit */
+				int yy_len = yyr2[ yy_n ];
+	
+				if ( ( yy_len & 01 )==0 )
+				{
+					yy_len >>= 1;
+					yyval = ( yy_pv -= yy_len )[1];	/* $$ = $1 */
+					yy_state = yypgo[ yy_n = yyr1[ yy_n ] ] +
+						( yy_ps -= yy_len )[0] + 1;
+					if ( yy_state >= YYLAST ||
+						yychk[ yy_state =
+						yyact[ yy_state ] ] != -yy_n )
+					{
+						yy_state = yyact[ yypgo[ yy_n ] ];
+					}
+					goto yy_stack;
+				}
+				yy_len >>= 1;
+				yyval = ( yy_pv -= yy_len )[1];	/* $$ = $1 */
+				yy_state = yypgo[ yy_n = yyr1[ yy_n ] ] +
+					( yy_ps -= yy_len )[0] + 1;
+				if ( yy_state >= YYLAST ||
+					yychk[ yy_state = yyact[ yy_state ] ] != -yy_n )
+				{
+					yy_state = yyact[ yypgo[ yy_n ] ];
+				}
+			}
+					/* save until reenter driver code */
+			yystate = yy_state;
+			yyps = yy_ps;
+			yypv = yy_pv;
 				
 
 
